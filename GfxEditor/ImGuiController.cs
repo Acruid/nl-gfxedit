14508aa63b5096241c71b25e2d45db3fc9cdebb3
Acruid <shatter66@gmail.com>
2023-04-24 20:39:58 -0700

    Prevent dragging windows from their body.

--- GfxEditor/ImGuiController.cs
+++ GfxEditor/ImGuiController.cs
@@ -56,6 +56,8 @@ public class ImGuiController : IDisposable
 
         io.BackendFlags |= ImGuiBackendFlags.RendererHasVtxOffset;
 
+        io.ConfigWindowsMoveFromTitleBarOnly = true;
+
         CreateDeviceResources();
         SetKeyMappings();
 
